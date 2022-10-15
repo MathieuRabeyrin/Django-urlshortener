cb6776efc7eed4cf6b00bd9d8bd09745c9e130bf
MathieuRabeyrin <m.rabeyrin0@gmail.com>
2022-10-15 20:49:44 +0200

    add postgresql

--- core/settings.py
+++ core/settings.py
@@ -81,12 +81,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'POSTGRESQL_DB' in os.environ.keys():
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("POSTGRESQL_DB"),
+            'USER': os.getenv("POSTGRESQL_USER"),
+            'PASSWORD': os.getenv("POSTGRESQL_PASSWORD"),
+            'HOST': os.getenv("POSTGRESQL_HOST"),
+            'PORT': os.getenv("POSTGRESQL_PORT"),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
